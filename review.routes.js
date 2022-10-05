e2870609ec1c7562d89e6a9be58c7a5afd0bc866
maxwell-paulo <maxpaulo.mps@gmail.com>
2022-10-05 19:16:10 -0300

    att

---
+++ review.routes.js
@@ -0,0 +1,158 @@
+import express from "express";
+import { ReviewModel } from "../model/review.model.js";
+import { UserModel } from "../model/user.model.js";
+import { GameModel } from "../model/game.model.js";
+import isAuth from "../middlewares/isAuth.js";
+import { isAdmin } from "../middlewares/isAdmin.js";
+import attachCurrentUser from "../middlewares/attachCurrentUser.js";
+
+const reviewRouter = express.Router();
+
+// Create review
+reviewRouter.post("/:id", isAuth, attachCurrentUser, async (req, res) => {
+  try {
+    const loggedUser = req.currentUser;
+
+    const createdReview = await ReviewModel.create({
+      ...req.body,
+      owner: loggedUser._id,
+      game: req.params.id,
+    });
+
+    await UserModel.findOneAndUpdate(
+      { _id: loggedUser._id },
+      { $push: { reviews: createdReview.id } }
+    );
+
+    await GameModel.findOneAndUpdate(
+      { _id: req.params.id },
+      { $push: { reviews: createdReview._id } }
+    );
+
+    return res.status(200).json(createdReview);
+  } catch (err) {
+    console.log(err);
+    ß;
+    return res.status(500).json(err);
+  }
+});
+
+// Read one game
+reviewRouter.get("/:id", isAuth, attachCurrentUser, async (req, res) => {
+  try {
+    const review = await ReviewModel.findOne({ _id: req.params.id })
+      .populate("owner")
+      .populate("game");
+
+    return res.status(200).json(review);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+//  Update review
+reviewRouter.put("/:id", isAuth, attachCurrentUser, async (req, res) => {
+  try {
+    const loggedUser = req.currentUser;
+    const review = await ReviewModel.findOne({ _id: req.params.id });
+    console.log(String(loggedUser._id));
+    console.log(String(review.owner._id));
+    console.log(loggedUser.role);
+
+    if (
+      String(loggedUser._id) === String(review.owner._id) ||
+      loggedUser.role === "ADMIN"
+    ) {
+      const editReview = await ReviewModel.findOneAndUpdate(
+        { _id: req.params.id },
+        { ...req.body },
+        { new: true, runValidators: true }
+      );
+      return res.status(200).json(editReview);
+    } else {
+      return res.status(401).json({ msg: "User unauthorized." });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+// User like the review
+reviewRouter.patch("/:id", isAuth, attachCurrentUser, async (req, res) => {
+  try {
+    const loggedUser = req.currentUser;
+    const review = await ReviewModel.findOne({ _id: req.params.id });
+
+    if (review.userLikeThis.includes(loggedUser._id)) {
+      await ReviewModel.findOneAndUpdate(
+        { _id: req.params.id },
+        { $pull: { userLikeThis: loggedUser._id } }
+      );
+
+      await UserModel.findOneAndUpdate(
+        { _id: loggedUser._id },
+        { $pull: { likeReviews: game._id } }
+      );
+
+      return res.status(200).json(review);
+    }
+    const userLike = await ReviewModel.findOneAndUpdate(
+      { _id: req.params.id },
+      { $push: { userLikeThis: loggedUser._id } }
+    );
+
+    await UserModel.findOneAndUpdate(
+      { _id: loggedUser._id },
+      { $push: { likeReviews: review._id } }
+    );
+
+    return res.status(200).json(userLike);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+// Delete review
+reviewRouter.delete(
+  "/:id",
+  isAuth,
+  attachCurrentUser,
+
+  async (req, res) => {
+    try {
+      const loggedUser = req.currentUser;
+      const review = await ReviewModel.findOne({ _id: req.params.id });
+
+      if (
+        String(loggedUser._id) === String(review.owner._id) ||
+        loggedUser.role === "ADMIN"
+      ) {
+        const deletedReview = await ReviewModel.deleteOne({
+          _id: req.params.id,
+        });
+
+        await GameModel.findOneAndUpdate(
+          { _id: review.game },
+          { $pull: { reviews: req.params.id } }
+        );
+
+        await UserModel.findOneAndUpdate(
+          { _id: review.owner },
+          { $pull: { reviews: req.params.id } }
+        );
+
+        return res.status(200).json(deletedReview);
+      } else {
+        return res.status(401).json({ msg: "User unauthorized" });
+      }
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  }
+);
+
+export { reviewRouter };
---
+++ user.routes.js
@@ -0,0 +1,236 @@
+import express from "express";
+import { generateToken } from "../config/jwt.config.js";
+import isAuth from "../middlewares/isAuth.js";
+import attachCurrentUser from "../middlewares/attachCurrentUser.js";
+import { isAdmin } from "../middlewares/isAdmin.js";
+import { UserModel } from "../model/user.model.js";
+
+import bcrypt from "bcrypt";
+import { gameRouter } from "./game.routes.js";
+import { GameModel } from "../model/game.model.js";
+import { ReviewModel } from "../model/review.model.js";
+
+const SALT_ROUNDS = 10;
+
+const userRouter = express.Router();
+
+// User Singup
+userRouter.post("/signup", async (req, res) => {
+  try {
+    const { password } = req.body;
+
+    if (
+      !password ||
+      !password.match(
+        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/gm
+      )
+    ) {
+      return res.status(400).json({
+        msg: "Email ou senha invalidos. Verifique se ambos atendem as requisições.",
+      });
+    }
+
+    const salt = await bcrypt.genSalt(SALT_ROUNDS);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const createdUser = await UserModel.create({
+      ...req.body,
+      passwordHash: hashedPassword,
+    });
+
+    delete createdUser._doc.passwordHash;
+    return res.status(201).json(createdUser);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+// User login
+userRouter.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await UserModel.findOne({ email: email });
+
+    if (!user) {
+      return res.status(404).json({ msg: "Email ou senha invalidos." });
+    }
+
+    if (await bcrypt.compare(password, user.passwordHash)) {
+      const token = generateToken(user);
+
+      return res.status(200).json({
+        user: {
+          name: user.name,
+          email: user.email,
+          _id: user._id,
+          role: user.role,
+        },
+        token: token,
+      });
+    } else {
+      return res.status(401).json({ msg: "Email ou senha invalidos." });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+// User Read
+userRouter.get("/profile", isAuth, attachCurrentUser, async (req, res) => {
+  try {
+    const loggedUser = req.currentUser;
+
+    const userData = await UserModel.findOne({ _id: loggedUser._id })
+      .populate("games")
+      .populate("reviews")
+      .populate("favoriteGames");
+
+    return res.status(200).json(userData);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+// Read All users
+userRouter.get(
+  "/users",
+  isAuth,
+  attachCurrentUser,
+  isAdmin,
+  async (req, res) => {
+    try {
+      const allUsers = await UserModel.find();
+
+      return res.status(200).json(allUsers);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  }
+);
+
+// User update
+userRouter.put("/:id", isAuth, attachCurrentUser, async (req, res) => {
+  try {
+    const loggedUser = req.currentUser;
+
+    if (
+      String(loggedUser._id) === req.params.id ||
+      loggedUser.role === "ADMIN"
+    ) {
+      const editUser = await UserModel.findByIdAndUpdate(
+        { _id: req.params.id },
+        { ...req.body },
+        { new: true, runValidators: true }
+      );
+
+      return res.status(200).json(editUser);
+    } else {
+      return res.status(401).json({ msg: "User unauthorized." });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+//User Admin role
+userRouter.put(
+  "/att-role/:id",
+  isAuth,
+  attachCurrentUser,
+  isAdmin,
+  async (req, res) => {
+    try {
+      const newAdm = await UserModel.findOneAndUpdate(
+        { _id: req.params.id },
+        { role: "ADMIN" },
+        { new: true, runValidators: true }
+      );
+
+      return res.status(200).json(newAdm);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  }
+);
+
+// User delete
+userRouter.delete("/:id", isAuth, attachCurrentUser, async (req, res) => {
+  try {
+    const loggedUser = req.currentUser;
+    const allGames = await GameModel.find().populate("reviews");
+
+    // console.log("%j", allGames);
+
+    if (
+      String(loggedUser._id) === req.params.id ||
+      loggedUser.role === "ADMIN"
+    ) {
+      // delete all user games
+      loggedUser.games.forEach(async (current) => {
+        await GameModel.deleteOne({ _id: current });
+      });
+
+      // // delete all user reviews from games
+
+      allGames.forEach(async (currentKey) => {
+        currentKey.reviews.forEach(async (currentReview) => {
+          if (String(currentReview.owner) === req.params.id) {
+            await GameModel.findOneAndUpdate(
+              { _id: currentReview.game },
+              { $pull: { reviews: currentReview._id } }
+            );
+          }
+        });
+      });
+
+      //  delete all user reviews
+      loggedUser.reviews.forEach(async (current) => {
+        await ReviewModel.deleteOne({ _id: current });
+      });
+
+      // delete all user likes from games
+      loggedUser.likeGames.forEach(async (current) => {
+        await GameModel.findOneAndUpdate(
+          { _id: current },
+          { $pull: { userLikeThis: loggedUser._id } }
+        );
+      });
+
+      // delete all user favorites from games
+      loggedUser.favoriteGames.forEach(async (current) => {
+        await GameModel.findOneAndUpdate(
+          { _id: current },
+          { $pull: { userFavoriteGame: loggedUser._id } }
+        );
+      });
+
+      // delete all user likes from review
+      loggedUser.likeReviews.forEach(async (current) => {
+        await ReviewModel.findOneAndUpdate(
+          { _id: current },
+          { $pull: { userLikeThis: loggedUser._id } }
+        );
+      });
+
+      // delete user
+      const deletedUser = await UserModel.deleteOne({ _id: req.params.id });
+
+      return res.status(200).json(deletedUser);
+    } else {
+      return res.status(401).json({ msg: "User unauthorized." });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+export { userRouter };
