c518261f4a1b7620ec28dc8d4ddae3d82f2fb420
Maxwell Paulo <maxpaulo.mps@gmail.com>
2023-02-22 16:48:37 -0300

    review model att

--- model/review.model.js
+++ model/review.model.js
@@ -4,13 +4,13 @@ const reviewSchema = new Schema({
   owner: { type: Schema.Types.ObjectId, ref: "User" },
   game: { type: Schema.Types.ObjectId, ref: "Game" },
   createdAt: { type: Date, default: Date.now() },
-  rates: {
-    graphics: { type: Number, required: true },
+  rates: [
+    {graphics: { type: Number, required: true },
     soundEffects: { type: Number, required: true },
     playability: { type: Number, required: true },
     fun: { type: Number, required: true },
-    replayability: { type: Number, required: true },
-  },
+    replayability: { type: Number, required: true },},
+  ],
   userEvaluation: { type: String },
   userLikeThis: [{ type: Schema.Types.ObjectId, ref: "User" }],
 });
